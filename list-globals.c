d30c77f40515f281e1d0590b1a3a0183f497c13f
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-01-13 22:07:46 +0100

    Improved clang-format

--- list-globals.c
+++ list-globals.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
+struct person {
+	int age;
+};
+
 static void wl_registry_global(void *data, struct wl_registry *registry,
-		uint32_t name, const char *interface, uint32_t version) {
+		uint32_t name, const char *interface, uint32_t version)
+{
 	fprintf(stderr, "%d: %s (%d)\n", name, interface, version);
 }
 
 static void wl_registry_global_remove(void *data,
-		struct wl_registry *wl_registry, uint32_t name) {}
+		struct wl_registry *wl_registry, uint32_t name)
+{
+}
 
 static const struct wl_registry_listener wl_registry_listener = {
 		.global = wl_registry_global,
 		.global_remove = wl_registry_global_remove,
 };
 
-int main(void) {
+int main(void)
+{
 	struct wl_display *wl_display = wl_display_connect(NULL);
 	struct wl_registry *wl_registry = wl_display_get_registry(wl_display);
 	wl_registry_add_listener(wl_registry, &wl_registry_listener, NULL);
