aab114b6ca7384a28bc35fedc1115b5a089b9ded
Łukasz Adamczak <14021+czak@users.noreply.github.com>
2024-01-14 14:12:52 +0100

    Progress towards decorations.c

--- decorations.c
+++ decorations.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <unistd.h>
@@ -5,81 +6,74 @@
 
 #include "protocols/xdg-decoration-unstable-v1.h"
 #include "protocols/xdg-shell.h"
+#include "protocols/single-pixel-buffer-v1.h"
+#include "protocols/viewporter.h"
 
 struct app_state {
-	struct wl_surface *surface;
-	struct wl_buffer *buffer;
+	// Wayland globals
+	struct wl_display *wl_display;
+	struct wl_registry *wl_registry;
+	struct wl_shm *wl_shm;
+	struct wl_compositor *wl_compositor;
+	struct xdg_wm_base *xdg_wm_base;
+	struct wp_viewporter *wp_viewporter;
+	struct wp_single_pixel_buffer_manager_v1 *wp_single_pixel_buffer_manager_v1;
+	struct zxdg_decoration_manager_v1 *zxdg_decoration_manager_v1;
+
+	// Surface & roles
+	struct wl_surface *wl_surface;
+	struct xdg_surface *xdg_surface;
+	struct xdg_toplevel *xdg_toplevel;
+
+	// Surface content & transform
+	struct wl_buffer *wl_buffer;
+	struct wp_viewport *wp_viewport;
+
+	// App state
+	int running;
 };
 
-// globals
-static struct wl_shm *shm = NULL;
-static struct wl_compositor *compositor = NULL;
-static struct xdg_wm_base *xdg_wm_base = NULL;
-static struct zxdg_decoration_manager_v1 *decoration_manager = NULL;
-
-// --- drawing ---
-
-static struct wl_buffer *draw_frame()
-{
-	const int width = 256, height = 256;
-	int stride = width * 4;
-	int size = stride * height;
-
-	int fd = memfd_create("buffer-pool", 0);
-	ftruncate(fd, size);
-	uint32_t *data =
-			mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-	struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
-	struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool, 0, width, height,
-			stride, WL_SHM_FORMAT_XRGB8888);
-	wl_shm_pool_destroy(pool);
-	close(fd);
-
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			uint8_t n = x ^ y;
-			data[y * width + x] = (n << 16) + (n << 8) + n;
-		}
-	}
-
-	munmap(data, size);
-
-	return buffer;
-}
-
-// --- wl_registry callbacks ---
-
-// https://wayland.app/protocols/wayland#wl_registry:event:global
 static void registry_global(void *data, struct wl_registry *registry,
 		uint32_t name, const char *interface, uint32_t version)
 {
+	struct app_state *app = data;
+
+	// clang-format off
 	if (strcmp(interface, wl_shm_interface.name) == 0) {
-		shm = wl_registry_bind(registry, name, &wl_shm_interface, 1);
+		app->wl_shm =
+				wl_registry_bind(registry, name, &wl_shm_interface, 1);
 	}
 
 	else if (strcmp(interface, wl_compositor_interface.name) == 0) {
-		compositor =
+		app->wl_compositor =
 				wl_registry_bind(registry, name, &wl_compositor_interface, 4);
 	}
 
 	else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
-		xdg_wm_base =
+		app->xdg_wm_base =
 				wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
 	}
 
-	else if (strcmp(interface, zxdg_decoration_manager_v1_interface.name) ==
-			 0) {
-		decoration_manager = wl_registry_bind(registry, name,
-				&zxdg_decoration_manager_v1_interface, 1);
+	else if (strcmp(interface, wp_viewporter_interface.name) == 0) {
+		app->wp_viewporter =
+				wl_registry_bind(registry, name, &wp_viewporter_interface, 1);
 	}
+
+	else if (strcmp(interface, wp_single_pixel_buffer_manager_v1_interface.name) == 0) {
+		app->wp_single_pixel_buffer_manager_v1 =
+				wl_registry_bind(registry, name, &wp_single_pixel_buffer_manager_v1_interface, 1);
+	}
+
+	else if (strcmp(interface, zxdg_decoration_manager_v1_interface.name) == 0) {
+		app->zxdg_decoration_manager_v1 =
+				wl_registry_bind(registry, name, &zxdg_decoration_manager_v1_interface, 1);
+	}
+	// clang-format on
 }
 
-// https://wayland.app/protocols/wayland#wl_registry:event:global_remove
 static void registry_global_remove(void *data, struct wl_registry *registry,
 		uint32_t name)
 {
-	// do nothing
 }
 
 static const struct wl_registry_listener registry_listener = {
@@ -87,59 +81,60 @@ static const struct wl_registry_listener registry_listener = {
 	.global_remove = registry_global_remove,
 };
 
-// --- xdg_surface callbacks ---
-
-// https://wayland.app/protocols/xdg-shell#xdg_surface:event:configure
 static void xdg_surface_configure(void *data, struct xdg_surface *xdg_surface,
 		uint32_t serial)
 {
+	struct app_state *app = data;
+
 	xdg_surface_ack_configure(xdg_surface, serial);
 
-	struct app_state *state = data;
-	wl_surface_attach(state->surface, state->buffer, 0, 0);
-	wl_surface_commit(state->surface);
+	wl_surface_attach(app->wl_surface, app->wl_buffer, 0, 0);
+	wl_surface_commit(app->wl_surface);
 }
 
 static const struct xdg_surface_listener xdg_surface_listener = {
 	.configure = xdg_surface_configure,
 };
 
-// ===== MAIN =====
+void app_init(struct app_state *app) {
+	app->wl_display = wl_display_connect(NULL);
+	app->wl_registry = wl_display_get_registry(app->wl_display);
+	wl_registry_add_listener(app->wl_registry, &registry_listener, app);
+	wl_display_roundtrip(app->wl_display);
+
+	assert(app->wl_shm &&
+			app->wl_compositor &&
+			app->xdg_wm_base &&
+			app->wp_viewporter &&
+			app->wp_single_pixel_buffer_manager_v1 &&
+			app->zxdg_decoration_manager_v1);
+
+	app->wl_surface = wl_compositor_create_surface(app->wl_compositor);
+	app->xdg_surface = xdg_wm_base_get_xdg_surface(app->xdg_wm_base, app->wl_surface);
+	xdg_surface_add_listener(app->xdg_surface, &xdg_surface_listener, app);
+
+	app->xdg_toplevel = xdg_surface_get_toplevel(app->xdg_surface);
+	xdg_toplevel_set_title(app->xdg_toplevel, "Decoration sample");
+	xdg_toplevel_set_min_size(app->xdg_toplevel, 256, 256);
+	xdg_toplevel_set_max_size(app->xdg_toplevel, 256, 256);
+
+	uint32_t shade = UINT32_MAX * 0.3;
+	app->wl_buffer = wp_single_pixel_buffer_manager_v1_create_u32_rgba_buffer(
+			app->wp_single_pixel_buffer_manager_v1, shade, shade, shade,
+			UINT32_MAX);
+	app->wp_viewport = wp_viewporter_get_viewport(app->wp_viewporter, app->wl_surface);
+	wp_viewport_set_destination(app->wp_viewport, 256, 256);
+
+	wl_surface_commit(app->wl_surface);
+}
 
 int main(int argc, char *argv[])
 {
-	// Initialize and bind globals
-	struct wl_display *display = wl_display_connect(NULL);
-	struct wl_registry *registry = wl_display_get_registry(display);
-	wl_registry_add_listener(registry, &registry_listener, NULL);
-	wl_display_roundtrip(display);
+	struct app_state app;
 
-	// Init state
-	struct app_state state = {
-		.surface = wl_compositor_create_surface(compositor),
-		.buffer = draw_frame(),
-	};
+	app_init(&app);
 
-	struct xdg_surface *xdg_surface =
-			xdg_wm_base_get_xdg_surface(xdg_wm_base, state.surface);
-	xdg_surface_add_listener(xdg_surface, &xdg_surface_listener, &state);
-
-	// Give the xdg_toplevel role to the surface
-	struct xdg_toplevel *xdg_toplevel = xdg_surface_get_toplevel(xdg_surface);
-	xdg_toplevel_set_title(xdg_toplevel, "Decoration sample");
-	xdg_toplevel_set_min_size(xdg_toplevel, 256, 256);
-	xdg_toplevel_set_max_size(xdg_toplevel, 256, 256);
-
-	// Request server side decorations
-	struct zxdg_toplevel_decoration_v1 *xdg_toplevel_decoration =
-			zxdg_decoration_manager_v1_get_toplevel_decoration(
-					decoration_manager, xdg_toplevel);
-	zxdg_toplevel_decoration_v1_set_mode(xdg_toplevel_decoration,
-			ZXDG_TOPLEVEL_DECORATION_V1_MODE_SERVER_SIDE);
-
-	wl_surface_commit(state.surface);
-
-	while (wl_display_dispatch(display) != -1) {
+	while (wl_display_dispatch(app.wl_display) != -1) {
 	}
 
 	return 0;
